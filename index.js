6a7c7e18c50ae4c5232b4fc678cb02f0e8b10ab5
Rajan <krajan92946@gmail.com>
2024-04-25 02:46:32 +0530

    Rajan

---
+++ index.js
@@ -0,0 +1,6 @@
+let a = document.getElementById("time")
+setInterval(() => {
+    let s = new Date();
+    let b = s.toLocaleString();
+    a.innerHTML=b;
+},1000);
--- table.js
+++ table.js
@@ -1,6 +1,6 @@
 async function fun()
 {
-    let n= await fetch("https://jsonplaceholder.typicode.com/albums/1/photos")
+    let n= await fetch("http://localhost:3000/student")
     let p=await n.json()
     let s=""
     let q=document.getElementById("data")
@@ -8,11 +8,18 @@ async function fun()
     
     <tr>
     <td>${e.id}</td>
-    <td>${e.albumId}</td>
-    <td>${e.title}</td>
-    <td><img src="${e.url}"</td>
+    <td>${e.name}</td>
+    <td>${e.city}</td>
+    <td>${e.occupation}</td>
 
     `).join()
     q.innerHTML=v;
 }
 fun()
+
+let a=document.getElementById("time")
+setInterval(() => {
+    let b= new Date();
+    let s=b.toLocaleString();
+    a.innerHTML=s;
+},1000);
